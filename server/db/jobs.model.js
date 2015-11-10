a8023779180e476faf0ca47d7bc070074d429e34
RedWood <mackey91@gmail.com>
2015-11-09 23:49:55 -0800

    db setup

--- server/db/jobs.model.js
+++ server/db/jobs.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var JobSchema = new Schema({
-  task: {type: String},
+  job: {type: String},
   dynos: {type: String},
   frequency: {type: String},
   lastRun: {type: String},
