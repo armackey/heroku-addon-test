a0d0d436f6701404a7271b03f4a9d1780865e0f1
RedWood <mackey91@gmail.com>
2015-11-10 04:01:50 -0800

    toggle the edit and save button

--- client/directives/edit.directive.js
+++ client/directives/edit.directive.js
@@ -1,14 +1,28 @@
-angular
-  .module('app')
-    .directive('editItem', function() {
-      return {
-        restrict: 'E',
-        template: '<button ng-click="edit(id)" >edit</button ng-click="edit()">',
-        controller: 'Main',
-        link: function(scope, elem, attrs) {
-          scope.edit = function(id) {
-            console.log(id);
+(function() {
+
+  angular
+    .module('app')
+    .directive('editItem', editItem);
+
+       function editItem($http) {
+
+          return {
+            restrict: 'E',
+            template: '<button ng-hide="isEditing" ng-click="edit(todo)" >edit</button">' +
+                      '<button ng-show="isEditing" ng-click="saveEdit()"> save</button>',
+            controller: 'Main',
+            link: function(scope, elem, attrs) {
+              scope.isEditing = false;
+              scope.edit = function(todo) {
+                scope.isEditing = true;
+              };
+              scope.saveEdit = function(todo) {
+                $http.put('/editJob/' + todo._id).then(function() {
+                  scope.isEditing = false;
+                });
+              };
+            }
           };
         }
-      };
-    });
+
+})();
